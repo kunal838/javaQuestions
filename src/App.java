b98b22aab342e482d0e210b9a57760b0b4b96f8a
kunal patel <pkunal257@gmail.com>
2022-03-24 11:46:21 +0530

    pattern

--- src/App.java
+++ src/App.java
@@ -3,7 +3,7 @@ class App{
     
    public static void main(String[] args)
     {
-       ArrayQ1.mergesortedarray();
+       problems.pattern11();
      
        
 }
--- src/problems.java
+++ src/problems.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class problems {
     public static boolean isAutomorphica(int N)
     {
@@ -285,6 +287,25 @@ public void pattern9(){
     }
     
    
+}
+public static void pattern11(){
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter a number: ");
+    int n=in.nextInt();
+ 
+    for(int i=1;i<=n;i++) {
+      for(int j=i;j<=n-1;j++) {
+        System.out.print(" ");
+      }
+      for(int j=1;j<=i;j++) {
+        System.out.print(j);
+      }
+      for(int j=i-1;j>=1;j--) {
+        System.out.print(j);
+      }
+      System.out.println();
+    }
+  
 }
 
 public void pattern10(){
